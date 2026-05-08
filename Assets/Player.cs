5515b995824a0e9a0904abd5f037ccddf51c45f9
Athar Siddiqui <atharsiddiqui211995@gmail.com>
2026-05-08 17:56:26 +0530

    Jump and Movement mechanics added

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,10 +2,29 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    // Update is called once per frame
-    void Update()
+    private Rigidbody2D rb;
+
+    private float MoveInputX;
+    [SerializeField ]private float MoveSpeed=3f;
+    [SerializeField] private float JumpForce=5f;
+    private void Awake()
     {
-        rb.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * 5, rb.linearVelocityY);
+        rb = GetComponent<Rigidbody2D>();
     }
+    private void Update()
+    {
+        PlayerMovement();
+    }
+    private void PlayerMovement()
+    {
+        //Horizontal movement
+        MoveInputX = Input.GetAxisRaw("Horizontal");
+        rb.linearVelocity = new Vector2(MoveInputX * MoveSpeed, rb.linearVelocity.y);
+
+        //Jumping mechanics
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, JumpForce);
+    }
+
 }
